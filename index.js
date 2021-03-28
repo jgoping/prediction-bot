dd2c6a9130bedb21028718f86ab74dbd284f1eae
jgoping <jgoping@gmail.com>
2021-03-27 22:24:29 -0400

    feat: pass db object and predictions state to the bot commands

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
+const mysql = require('mysql');
+
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'XXXXXXXX',
+  database: 'predictions'
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to MySQL');
+});
 
 const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
 const botCommands = require('./commands');
 
-console.log(botCommands);
+const predictions = {open: false, yes: [], no: []};
+
 Object.keys(botCommands).map(key => {
   bot.commands.set(botCommands[key].name, botCommands[key]);
 });
@@ -19,8 +34,8 @@ bot.on('message', async (msg) => {
   if (!bot.commands.has(command)) return;
 
   try {
-    bot.commands.get(command).execute(msg, args);
-  } catch {
+    bot.commands.get(command).execute(msg, args, db, predictions);
+  } catch (error) {
     console.error(error);
     msg.reply('There was an error trying to execute that command.');
   }
